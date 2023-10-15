579486861c813fbe8130d42fc3afca7f3c25c801
김철 <dmsghs92@gmail.com>
2023-10-15 20:47:33 +0900

    Use AuthInfo in Controller

--- src/main/java/com/rest/api/controller/UserController.java
+++ src/main/java/com/rest/api/controller/UserController.java
@@ -4,6 +4,8 @@ import com.rest.api.util.ApiResponse;
 import com.rest.api.dto.UserDTO;
 import com.rest.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -17,15 +19,15 @@ public class UserController {
     }
 
     @PostMapping("/user/register")
-    public ApiResponse<String> save(@ModelAttribute UserDTO userDTO){
-        System.out.println("userDTO = " + userDTO);
+    public ApiResponse<String> save(@ModelAttribute UserDTO userDTO, Authentication authentication){
         userService.addUser(userDTO);
-        return ApiResponse.createSuccess("userDTO =" + userDTO);
+        return ApiResponse.createSuccess("userDTO =" + userDTO + "\n 유저 인증 이메일 정보:" + authentication.getName());
     }
 
     @PostMapping("/user/login")
     public ApiResponse<String> login(@ModelAttribute UserDTO userDTO){
         String jwt = userService.login(userDTO);
+
         if (jwt != null){
             return ApiResponse.createSuccess("Success with jwt: " + jwt);
         }else{
